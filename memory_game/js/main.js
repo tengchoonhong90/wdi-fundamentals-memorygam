cc125a7584007c73a4e3b8a94acb5ca7f5174c2a
Teng Choon Hong <tengchoonhong_90@outlook.com>
2018-11-24 19:37:18 +0800

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,20 +1,23 @@
 var cards = ["King", "King", "Queen", "Queen"];
 var cardsInPlay = [];
-var cardOne = cards[0];
 
-cardsInPlay.push(cardOne);
-
-console.log('You flipped ' + cardOne);
-
-var cardTwo = cards[1];
-cardsInPlay.push(cardTwo);
-
-console.log('You flipped ' + cardTwo);
-
-if ( cardsInPlay.length === 2 ) {
-	if ( cardsInPlay[0] === cardsInPlay[1] ) {
-		alert('You found a match!');
+var checkForMatch = function() {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+		console.log("You found a match!");
 	} else {
-		alert('Sorry, Try again');
+		console.log("Sorry, try again.");
 	}
 }
+
+var flipCard = function(cardId) {
+	console.log("You flipped " + cards[cardId]);
+	cardsInPlay.push(cards[cardId]);
+	checkForMatch();
+}
+
+
+
+
+
+
+
