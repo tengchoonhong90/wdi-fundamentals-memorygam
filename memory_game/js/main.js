a67b5e23043a427c811df9b2d6b8846bd75cb1e1
Teng Choon Hong <tengchoonhong_90@outlook.com>
2018-11-24 12:54:44 +0800

    created game logic

---
+++ memory_game/js/main.js
@@ -0,0 +1,7 @@
+var cardOne = 'King of Diamonds';
+var cardTwo = 'King of Hearts';
+var cardThree = 'Queen of Diamonds';
+var cardFour = 'Queen of Hearts';
+
+console.log("You flipped " + cardOne);
+console.log("You flipped " + cardTwo);
