48570c83f7f78732da14c42d254227574052b3f8
Teng Choon Hong <tengchoonhong_90@outlook.com>
2018-11-25 15:23:37 +0800

    complete basic memory game

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,43 +1,59 @@
 var cards = [
 	{
-	suit: "Diamonds",
-	rank: "King",
-	cardImage: "images/king-of-diamonds.png",
+	suit: 'Diamonds',
+	rank: 'King',
+	cardImage: 'images/king-of-diamonds.png',
 	},
 	{
-	suit: "Diamonds",
-	rank: "Queen",
-	cardImage: "images/queen-of-diamonds.png",
+	suit: 'Diamonds',
+	rank: 'Queen',
+	cardImage: 'images/queen-of-diamonds.png',
 	},
 	{
-	suit: "Hearts",
-	rank: "King",
-	cardImage: "images/king-of-hearts.png",
+	suit: 'Hearts',
+	rank: 'King',
+	cardImage: 'images/king-of-hearts.png',
 	},
 	{
-	suit: "Hearts",
-	rank: "Queen",
-	cardImage: "images/queen-of-hearts.png",
+	suit: 'Hearts',
+	rank: 'Queen',
+	cardImage: 'images/queen-of-hearts.png',
 	}
 ];
+
 
 var cardsInPlay = [];
 
 var checkForMatch = function() {
 	if (cardsInPlay[0] === cardsInPlay[1]) {
-		console.log("You found a match!");
+		alert("You found a match!");
 	} else {
-		console.log("Sorry, try again.");
-	}
-}
+		alert("Sorry, try again.");
+	};
+};
 
-var flipCard = function(cardId) {
-	console.log("You flipped " + cards[cardId].rank);
+var flipCard = function() {
+	var cardId = this.getAttribute('data-id');
+	console.log('You flipped ' + cards[cardId].rank + ' ' + cards[cardId].suit);
 	cardsInPlay.push(cards[cardId].rank);
-	checkForMatch();
-	console.log("You flipped " + cards[cardId].cardImage);
-	console.log("You flipped " + cards[cardId].suit);
-}
+	this.setAttribute('src', cards[cardId].cardImage); 
+	if (cardsInPlay.length === 2) {
+		checkForMatch();
+	};
+};
+
+var createBoard = function() {
+	for (var i = 0; i < cards.length; i++) {
+		var cardElement = document.createElement('img');
+		cardElement.setAttribute('src', 'images/back.png');
+		cardElement.setAttribute('data-id', i);
+		cardElement.addEventListener('click', flipCard); 
+		document.getElementById('game-board').appendChild(cardElement); 
+	}
+};
+
+createBoard();
+
 
 
 
