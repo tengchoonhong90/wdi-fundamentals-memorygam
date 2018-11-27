8f3844aa1e3dd9a9a90f8876d0dd8afdb52f55b8
tengchoonhong90 <tengchoonhong_90@outlook.com>
2018-11-27 13:02:43 +0800

    add reset function and score counter

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -24,15 +24,35 @@ var cards = [
 
 // to define an empty array for to hold cards
 var cardsInPlay = [];
+var score = 0;
+var flippedCards = document.getElementsByClassName('flipped');
+
+// function to shuffle cards index in array
+function shuffle(cards) {
+    for (let i = cards.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [cards[i], cards[j]] = [cards[j], cards[i]];
+    }
+};
+
+// function to reset cards
+var resetCard = function() {
+    for (var i = 0; i < flippedCards.length; ++i) {
+        flippedCards[i].setAttribute('src', 'images/back.png');
+        cardsInPlay = [];
+        shuffle(cards);
+    }
+};
 
 // function to check for match and print text accordingly
 var checkForMatch = function() {
 	if (cardsInPlay[0] === cardsInPlay[1]) {
 		document.getElementById('fliptext').innerHTML = "You found a match!";
-	} else {	
+        ++score;
+        document.getElementById('score').innerHTML = "You got " + score + " matches";
+	} else {
 		document.getElementById('fliptext').innerHTML = "Sorry, try again.";
 	};
-	reset();
 };
 
 // function to flip card
@@ -40,7 +60,8 @@ var flipCard = function() {
 	var cardId = this.getAttribute('data-id');
 	console.log('You flipped ' + cards[cardId].rank + ' ' + cards[cardId].suit);
 	cardsInPlay.push(cards[cardId].rank);
-	this.setAttribute('src', cards[cardId].cardImage); 
+	this.setAttribute('src', cards[cardId].cardImage);
+    this.setAttribute('class', 'flipped')
 	if (cardsInPlay.length === 2) {
 		checkForMatch();
 	};
@@ -52,13 +73,17 @@ var createBoard = function() {
 		var cardElement = document.createElement('img');
 		cardElement.setAttribute('src', 'images/back.png');
 		cardElement.setAttribute('data-id', i);
-		cardElement.addEventListener('click', flipCard); 
-		document.getElementById('game-board').appendChild(cardElement); 
+		cardElement.addEventListener('click', flipCard);
+		document.getElementById('game-board').appendChild(cardElement);
 	}
 };
 
 // call createBoard function
 createBoard();
+
+//activate reset button on click
+document.getElementById('reset').addEventListener('click', resetCard);
+
 
 
 
